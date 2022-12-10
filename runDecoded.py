1428d20f4ef13ba35576d66bfc91dc8a1d6ac031
slopiano <slopiano@stevens.edu>
2022-12-10 11:43:08 -0500

    testing

--- runDecoded.py
+++ runDecoded.py
@@ -1,8 +1,10 @@
 import json
 import time
 import MRTD
+import unittest
 from MRTD import incode_string, checksum
 from Passport import Passport
+
 
 f = open('records_decoded.json')
 data = json.load(f)
@@ -10,7 +12,7 @@ data = json.load(f)
 records = data.get("records_decoded", [])
 f.close()
 
-
+# Calculates the runtime for the first 100 records
 first100 = records[0:100]
 start_time = time.time()
 for record in first100:
@@ -30,22 +32,9 @@ for record in first100:
     record2.get("personal_number"),checksum(record2.get("personal_number")))
     incode_string()
 print(f'Total time to complete first 100 is : {time.time()-start_time} seconds')
-# record = records[0]
 
-# record1 = record.get("line1")
-# record2 = record.get("line2")
 
-# print(record1.get("issuing_country"))
-# print(record1.get("last_name"))
-# print(record1.get("given_name").split(' '))
-# print('#################################################')
-# print(record2.get("passport_number"))
-# print(record2.get("country_code"))
-# print(record2.get("birth_date"))
-# print(record2.get("sex"))
-# print(record2.get("expiration_date"))
-# print(record2.get("personal_number"))
-
+# Calculates the run time for the records 1000 up to 10000 stepping 1000 each time.
 prev_i = 0
 start_time = time.time()
 for i in range(1000, 10001, 1000):
@@ -68,3 +57,68 @@ for i in range(1000, 10001, 1000):
         record2.get("personal_number"),checksum(record2.get("personal_number")))
         incode_string()
     print(f'Total time to complete {i} records is : {time.time()-start_time} seconds')
+
+
+f = open('records_encoded.json')
+data = json.load(f)
+# This function loads the given sample from the json file
+records_encoded = data.get("records_encoded", [])
+f.close()
+
+class TestForErrors(unittest.TestCase):
+    def test_first_100(self):
+        # Calculates the runtime for the first 100 records
+        first100 = records[0:100]
+        start_time = time.time()
+        idx=0
+        for record in first100:
+            record1 = record.get("line1")
+            record2 = record.get("line2")
+            name = record1.get("given_name").split(' ')
+            if len(name) < 2:
+                first_name = name[0]
+                middle_name = ""
+            else:
+                first_name = name[0]
+                middle_name = name[1]
+            MRTD.passport_class = Passport("P",record1.get("issuing_country"),record1.get("last_name"),first_name,middle_name,
+            record2.get("passport_number"),checksum(record2.get("passport_number")),record2.get("country_code"),
+            record2.get("birth_date"),checksum(record2.get("birth_date")),record2.get("sex"),
+            record2.get("expiration_date"),checksum(record2.get("expiration_date")),
+            record2.get("personal_number"),checksum(record2.get("personal_number")))
+            incode_string()
+            self.assertEqual(';'.join(MRTD.passport_string), records_encoded[idx])
+            idx+=1
+        print(f'Total time to complete first 100 with unit tests is : {time.time()-start_time} seconds')
+
+    def test_10000(self):
+        # Calculates the run time for the records 1000 up to 10000 stepping 1000 each time.
+        prev_i = 0
+        start_time = time.time()
+        for i in range(1000, 10001, 1000):
+            records_to_test = records[prev_i:i]
+            idx = prev_i
+            prev_i = i
+            for record in records_to_test:
+                record1 = record.get("line1")
+                record2 = record.get("line2")
+                name = record1.get("given_name").split(' ')
+                if len(name) < 2:
+                    first_name = name[0]
+                    middle_name = ""
+                else:
+                    first_name = name[0]
+                    middle_name = name[1]
+                MRTD.passport_class = Passport("P",record1.get("issuing_country"),record1.get("last_name"),first_name,middle_name,
+                record2.get("passport_number"),checksum(record2.get("passport_number")),record2.get("country_code"),
+                record2.get("birth_date"),checksum(record2.get("birth_date")),record2.get("sex"),
+                record2.get("expiration_date"),checksum(record2.get("expiration_date")),
+                record2.get("personal_number"),checksum(record2.get("personal_number")))
+                incode_string()
+                self.assertEqual(';'.join(MRTD.passport_string), records_encoded[idx])
+                idx+=1
+            print(f'Total time to complete {i} records with unit tests is : {time.time()-start_time} seconds')
+
+
+if __name__ == '__main__':
+    unittest.main()
--- runEncoded.py
+++ runEncoded.py
@@ -1,5 +1,6 @@
 import json
 import time
+import unittest
 import MRTD
 from MRTD import decode_string
 
@@ -25,3 +26,79 @@ for i in range(1000, 10001, 1000):
         MRTD.passport_string = record.split(';')
         decode_string()
     print(f'Total time to complete {i} records is : {time.time()-start_time} seconds')
+
+f = open('records_decoded.json')
+data = json.load(f)
+# This function loads the given sample from the json file
+records_decoded = data.get("records_decoded", [])
+f.close()
+
+# "issuing_country"
+# "last_name"
+# "given_name"
+
+# "passport_number"
+# "country_code"
+# "birth_date"
+# "sex"
+# "expiration_date"
+# "personal_number"
+
+class TestForErrors(unittest.TestCase):
+    def test_first_100(self):
+        first100 = records[0:100]
+        start_time = time.time()
+        idx = 0
+        for record in first100:
+            record_decoded = records_decoded[idx]
+            record_decoded1 = record_decoded.get("line1")
+            record_decoded2 = record_decoded.get("line2")
+            MRTD.passport_string = record.split(';')
+            decode_string()
+            self.assertEqual(MRTD.passport_class.c_code_top, record_decoded1.get("issuing_country"))
+            self.assertEqual(MRTD.passport_class.l_name, record_decoded1.get("last_name"))
+            if MRTD.passport_class.m_name == "": 
+                space = ""
+            else:
+                space = " "
+            self.assertEqual(MRTD.passport_class.f_name+space+MRTD.passport_class.m_name, record_decoded1.get("given_name"))
+            self.assertEqual(MRTD.passport_class.pass_num, record_decoded2.get("passport_number"))
+            self.assertEqual(MRTD.passport_class.c_code_bot, record_decoded2.get("country_code"))
+            self.assertEqual(MRTD.passport_class.birth_date, record_decoded2.get("birth_date"))
+            self.assertEqual(MRTD.passport_class.sex, record_decoded2.get("sex"))
+            self.assertEqual(MRTD.passport_class.expiration, record_decoded2.get("expiration_date"))
+            self.assertEqual(MRTD.passport_class.personal_number, record_decoded2.get("personal_number"))
+            idx+=1
+        print(f'Total time to complete first 100 with unit tests is : {time.time()-start_time} seconds')
+        
+    def test_10000(self):
+        start_time = time.time()
+        prev_i = 0
+        for i in range(1000, 10001, 1000):
+            records_to_test = records[prev_i:i]
+            idx = prev_i
+            prev_i = i
+            for record in records_to_test:
+                record_decoded = records_decoded[idx]
+                record_decoded1 = record_decoded.get("line1")
+                record_decoded2 = record_decoded.get("line2")
+                MRTD.passport_string = record.split(';')
+                decode_string()
+                self.assertEqual(MRTD.passport_class.c_code_top, record_decoded1.get("issuing_country"))
+                self.assertEqual(MRTD.passport_class.l_name, record_decoded1.get("last_name"))
+                if MRTD.passport_class.m_name == "": 
+                    space = ""
+                else:
+                    space = " "
+                self.assertEqual(MRTD.passport_class.f_name+space+MRTD.passport_class.m_name, record_decoded1.get("given_name"))
+                self.assertEqual(MRTD.passport_class.pass_num, record_decoded2.get("passport_number"))
+                self.assertEqual(MRTD.passport_class.c_code_bot, record_decoded2.get("country_code"))
+                self.assertEqual(MRTD.passport_class.birth_date, record_decoded2.get("birth_date"))
+                self.assertEqual(MRTD.passport_class.sex, record_decoded2.get("sex"))
+                self.assertEqual(MRTD.passport_class.expiration, record_decoded2.get("expiration_date"))
+                self.assertEqual(MRTD.passport_class.personal_number, record_decoded2.get("personal_number"))
+                idx+=1
+            print(f'Total time to complete {i} records with unit tests is : {time.time()-start_time} seconds')
+
+if __name__ == '__main__':
+    unittest.main()
